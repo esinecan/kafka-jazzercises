c2ff4cafd1d2409fdbf03ed5f97eed7dca3ce495
Eren Can Sinecan <esinecan@gmail.com>
2024-10-17 06:57:35 +0200

    Add files via upload

---
+++ src/test/java/com/eren/kafka_java/KafkaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eren.kafka_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
